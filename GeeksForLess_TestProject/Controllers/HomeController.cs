df6a9f773132a94dc7e34d1665699ad843d37b95
Serhii Zemliakov <seriyzemlyakov@gmail.com>
2024-01-25 21:22:35 +0200

    Add project files.

---
+++ GeeksForLess_TestProject/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+using GeeksForLess_TestProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+namespace GeeksForLess_TestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHost;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHost)
+        {
+            _logger = logger;
+            _webHost = webHost;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                ViewBag.Message = "No file selected.";
+                return View();
+            }
+
+            var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (fileExtension != ".json")
+            {
+                ViewBag.Message = "Invalid file type. Please upload a .json file.";
+                return View();
+            }
+
+            string uploadsFolder = Path.Combine(_webHost.WebRootPath,"uploads");
+
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            } else
+            {
+                Directory.Delete(uploadsFolder, true);
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+
+            string fileName = Path.GetFileName(file.FileName);
+            string fileSavePath = Path.Combine(uploadsFolder, fileName);
+
+            using (FileStream stream = new FileStream(fileSavePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            ViewBag.Message = fileName + " uploaded succesfully";
+
+            return View();
+        }
+
+        public IActionResult Tree()
+        {
+            string uploadsFolder = Path.Combine(_webHost.WebRootPath, "uploads");
+            
+            var webClient = new WebClient();
+            string fileName = Directory.GetFiles(uploadsFolder).First();
+            var json = webClient.DownloadString(fileName);
+            DataTable dataTable = HomeViewModel.ConvertToDataTable(json);
+
+            HomeViewModel.UploadDataTableToSql(dataTable, "testTable");
+
+            return View(dataTable);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GeeksForLess_TestProject/Models/HomeViewModel.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace GeeksForLess_TestProject.Models
+{
+    public class HomeViewModel
+    {
+        public static DataTable ConvertToDataTable(string json)
+        {
+            var jsonObject = JObject.Parse(json);
+            DataTable dataTable = new DataTable();
+            AddColumns(jsonObject, dataTable, "");
+            AddRows(jsonObject, dataTable, "");
+            return dataTable;
+        }
+
+        private static void AddColumns(JObject jsonObject, DataTable dataTable, string prefix)
+        {
+            foreach (var property in jsonObject.Properties())
+            {
+                var columnName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix}_{property.Name}";
+
+                if (property.Value.Type == JTokenType.Object)
+                {
+                    var nestedObject = (JObject)property.Value;
+                    AddColumns(nestedObject, dataTable, columnName);
+                }
+                else
+                {
+                    dataTable.Columns.Add(columnName);
+                }
+            }
+        }
+
+        private static void AddRows(JObject jsonObject, DataTable dataTable, string prefix)
+        {
+            var row = dataTable.NewRow();
+
+            foreach (var property in jsonObject.Properties())
+            {
+                var columnName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix}_{property.Name}";
+
+                if (property.Value.Type == JTokenType.Object)
+                {
+                    var nestedObject = (JObject)property.Value;
+                    AddRows(nestedObject, dataTable, columnName);
+                }
+                else
+                {
+                    row[columnName] = property.Value.ToString();
+                }
+            }
+
+            dataTable.Rows.Add(row);
+        }
+
+        public static void UploadDataTableToSql(DataTable dataTable, string destinationTableName)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\Programming\\c#\\projects\\ASP.NET Core\\GeeksForLess_TestProject\\GeeksForLess_TestProject\\Database.mdf\";Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True";
+
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand command = new SqlCommand($"DROP TABLE {destinationTableName}", sqlConnection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                CreateDestinationTable(sqlConnection, dataTable, destinationTableName);
+
+                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(sqlConnection))
+                {
+                    sqlBulkCopy.DestinationTableName = destinationTableName;
+
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        sqlBulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                    }
+
+                    sqlBulkCopy.WriteToServer(dataTable);
+                }
+            }
+        }
+
+        private static void CreateDestinationTable(SqlConnection sqlConnection, DataTable dataTable, string tableName)
+        {
+            using (SqlCommand command = new SqlCommand(BuildCreateTableQuery(dataTable, tableName), sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private static string BuildCreateTableQuery(DataTable dataTable, string tableName)
+        {
+            string createTableQuery = $"CREATE TABLE {tableName} (";
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                createTableQuery += $"{column.ColumnName} {GetSqlDbType(column.DataType)}, ";
+            }
+            
+            createTableQuery = createTableQuery.TrimEnd(',', ' ') + ")";
+
+            return createTableQuery;
+        }
+
+        private static string GetSqlDbType(Type dataType)
+        {
+            if (dataType == typeof(int))
+                return "INT";
+            else if (dataType == typeof(string))
+                return "NVARCHAR(MAX)";
+
+            return "NVARCHAR(MAX)";
+        }
+    }
+}
