c05c970895c17e99b2b8ebfb7f3dc81d18d9cb3d
Serhii Zemliakov <seriyzemlyakov@gmail.com>
2024-01-26 21:49:43 +0200

    Small changes

--- GeeksForLess_TestProject/Controllers/HomeController.cs
+++ GeeksForLess_TestProject/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 using GeeksForLess_TestProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Diagnostics;
 using System.Net;
-using System.Text.Json;
 
 namespace GeeksForLess_TestProject.Controllers
 {
